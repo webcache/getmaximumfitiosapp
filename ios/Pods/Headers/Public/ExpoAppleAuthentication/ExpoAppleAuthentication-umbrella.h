0718879dda2c9ab17ace6b215909c577002660bd
webcache <a.cash@mac.com>
2025-08-03 14:53:13 -0500

    update to add pods

---
+++ ios/Pods/Headers/Public/ExpoAppleAuthentication/ExpoAppleAuthentication-umbrella.h
@@ -0,0 +1,1 @@
+../../../Target Support Files/ExpoAppleAuthentication/ExpoAppleAuthentication-umbrella.h
---
+++ ios/Pods/Target Support Files/ExpoAppleAuthentication/ExpoAppleAuthentication-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ExpoAppleAuthentication : NSObject
+@end
+@implementation PodsDummy_ExpoAppleAuthentication
+@end
